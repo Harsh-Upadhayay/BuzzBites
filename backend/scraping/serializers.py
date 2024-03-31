220164e6a44023f75324440a524432076ed266b4
Harsh_324 <harshravi324@gmail.com>
2024-03-31 13:59:20 +0530

    some changes

--- backend/scraping/serializers.py
+++ backend/scraping/serializers.py
@@ -11,15 +11,8 @@ class MemeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class NewsSerializer(serializers.ModelSerializer):
-    # news_id = serializers.CharField()
-    # news_url = serializers.CharField()
-    # news_title = serializers.CharField()
-    # news_description = serializers.CharField()
-    # news_summary = serializers.CharField()
-    # created_at = serializers.DateTimeField()
-    # updated_at = serializers.DateTimeField()
     class Meta:
-        model = None  # Setting to None as we will define it dynamically based on the queryset
+        model = None
 
 
 class CricbuzzNewsSerializer(NewsSerializer):
